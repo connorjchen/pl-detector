4e8aa4e7e7f1e6b6720dc346e97a2b8af4a08878
Darren Key <dyk34@cornell.edu>
2022-04-24 15:10:04 -0400

    venv added

--- python-server/main.py
+++ python-server/main.py
@@ -8,5 +8,10 @@ def scuffed_editor():
     return render_template("main.html")
 
 
+# @app.route("/api/backend/")
+# def scuffed_editor():
+#     return render_template("main.html")
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
